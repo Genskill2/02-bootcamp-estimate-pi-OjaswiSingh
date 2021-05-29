8d1bd8a61594c8a1e9d79d3977b3d2391b50bd90
OjaswiSingh <ojaswisinghnaruto@gmail.com>
2021-05-30 00:35:23 +0530

    assignment_sol1

--- estimate.py
+++ estimate.py
@@ -1,6 +1,18 @@
 import math
 import unittest
+import random
 
+
+#Function wallis
+
+def wallis(n):
+    pi = 1
+    while n:
+        pi *= (4*n**2)/(4*n**2-1)
+        n = n-1
+    return 2*pi 
+    
+    
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
@@ -11,6 +23,22 @@ class TestWallis(unittest.TestCase):
         for i in range(500, 600):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+
+
+# monte_carlo function
+
+def monte_carlo(i):
+    n = i
+    j = 0
+    while n:
+        x = random.random()
+        y = random.random()
+        dist = (x**2+y**2)**0.5
+        if dist < 1:
+            j = j+1
+        n -= 1
+    pi = 4*(j/i)
+    return pi
 
 
 class TestMC(unittest.TestCase):
